7ca31df1f1a4d3d10635d60d2ac1a8a8912640c1
Kamil Smoczynski <kamilsmoczynski9309@gmail.com>
2022-12-30 03:56:25 +0100

    finish converter

--- script.js
+++ script.js
@@ -5,22 +5,49 @@ const resetBtn = document.querySelector('.reset')
 const changeBtn = document.querySelector('.change')
 const one = document.querySelector('.one')
 const two = document.querySelector('.two')
-const cel = '°C'
+const reg = /[0-9]/
 
 let fahrenheit
 let celsius
 
-// T(°F) = T(°C) × 1.8 + 32
-// T(°C) = (T(°F) - 32) / 1.8
 
 const change = () => {
-	if ((one.textContent == '°C')) {
+	if (one.textContent == '°C') {
 		one.textContent = '°F'
 		two.textContent = '°C'
+		result.textContent = ''
 	} else {
 		one.textContent = '°C'
 		two.textContent = '°F'
+		result.textContent = ''
+	}
+	if (converter.value !== '') {
+		
+
+	}else {
+		result.textContent = 'Musisz podać jakąś liczbę'
+	}
+}
+
+const convF = () => {
+	if (one.textContent == '°C') {
+		fahrenheit = converter.value * 1.8 + 32
+		result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+		converter.value = ''
+	} else {
+		celsius = (converter.value - 32) / 1.8
+		result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
+		converter.value = ''
+	}
+}
+
+const reset = () => {
+	if (converter.value !== 'reg') {
+		converter.value = ''
+		result.textContent = ''
 	}
 }
 
 changeBtn.addEventListener('click', change)
+convBtn.addEventListener('click', convF)
+resetBtn.addEventListener('click', reset)
